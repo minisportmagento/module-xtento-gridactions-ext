799ebc887b190fd77979ec7a2b05707e20ffcef3
Vitaliy Dmytruk <vitaliy@swissuplabs.com>
2020-11-11 11:47:14 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Minisport_XtentoGridActionsExt',
+    __DIR__
+);
